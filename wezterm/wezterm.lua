4007317baca25300a8ceeca35d665b33838de368
Johan Friis <johan@slowmail.it>
2023-11-25 17:54:24 +0100

    wezterm: remove fancy colorscheme changer

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,39 +8,7 @@ end
 
 -- // COLOR SCHEME
 
-local function get_appearance()
-	if wezterm.gui then
-		return wezterm.gui.get_appearance()
-	end
-	return 'Dark'
-end
-
-local function scheme_for_appearance(appearance)
-	if appearance:find 'Dark' then
-		return 'rose-pine-moon'
-	else
-		return 'rose-pine-dawn'
-	end
-end
-
-local function colors_for_appearance(appearance)
-	if appearance:find 'Dark' then
-		return {
-			selection_fg = '#e0def4',
-			selection_bg = '#44415a'
-		}
-	else
-		return {
-			selection_fg = '#575279',
-			selection_bg = '#cecacd'
-		}
-	end
-end
-
-config.color_scheme = scheme_for_appearance(get_appearance())
-config.colors = colors_for_appearance(get_appearance())
-
---config.color_scheme = 'rose-pine-moon'
+config.color_scheme = 'rose-pine-moon'
 config.colors = {
   selection_fg = '#e0def4',
   selection_bg = '#44415a'
