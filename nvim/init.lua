19c08a246438959c824772c10c8e60123ab427fb
Johan Friis <johan@slowmail.it>
2023-11-26 20:54:00 +0100

    nvim: disable a set of builtin plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,18 @@
 vim.loader.enable()
 
+-- vim.g.loaded_gzip = 1
+-- vim.g.loaded_rplugin = 1
+-- vim.g.loaded_tarPlugin = 1
+-- vim.g.loaded_tohtml = 1
+-- vim.g.loaded_tutor = 1
+-- vim.g.loaded_zipPlugin = 1
+-- vim.g.loaded_editorconfig = 1
+-- vim.g.loaded_man = 1
+
 require('config')
+
+
+
+
 
 ---- vim: foldmethod=marker ts=2 sts=2 sw=2 et
