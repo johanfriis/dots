f67be23d5119f118e28364e52ad85d0fa0185ff3
Johan Friis <johan@slowmail.it>
2023-11-26 12:19:11 +0100

    nvim: set markdown files to be pandoc filetype

---
+++ nvim/ftdetect/pandoc.lua
@@ -0,0 +1,3 @@
+-- force markdown to be recognised as pandoc
+vim.cmd [[autocmd! filetypedetect BufNewFile,BufRead *.{md,markdown,mdown,mkd,mkdn}]]
+vim.cmd [[autocmd BufNewFile,BufRead,BufFilePost *.{md,markdown,mdown,mkd,mkdn} setlocal filetype=pandoc]]
