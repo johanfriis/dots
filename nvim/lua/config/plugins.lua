7f10002491977e2857a04a0deb8c70fc34fcd1b9
Johan Friis <johan@slowmail.it>
2023-11-27 22:11:07 +0100

    nvim: add mini.clue config

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -9,6 +9,8 @@ p.load('cmp')
 p.lazy('copilot', nil, { 'InsertEnter' })
 p.load('lspconfig')
 
+p.load('mini-clue')
+
 vim.api.nvim_create_user_command('LazyGit', function()
   vim.api.nvim_del_user_command('LazyGit')
   p.load('lazygit')
---
+++ nvim/lua/plugins/mini-clue.lua
@@ -0,0 +1,60 @@
+local clue = require('mini.clue')
+
+local main_clues = {
+  { mode = 'n', keys = '<Leader>b', desc = '+Buffer' },
+  { mode = 'n', keys = '<Leader>c', desc = '+Code' },
+  { mode = 'n', keys = '<Leader>e', desc = '+Explore' },
+  { mode = 'n', keys = '<Leader>f', desc = '+Find' },
+  { mode = 'n', keys = '<Leader>g', desc = '+Git' },
+  { mode = 'n', keys = '<Leader>l', desc = '+LSP' },
+  { mode = 'n', keys = '<Leader>t', desc = '+Toggle' },
+}
+
+clue.setup({
+  clues = {
+    main_clues,
+    clue.gen_clues.builtin_completion(),
+    clue.gen_clues.g(),
+    clue.gen_clues.marks(),
+    clue.gen_clues.registers(),
+    clue.gen_clues.windows({ submode_resize = true }),
+    clue.gen_clues.z(),
+  },
+  triggers = {
+    -- Leader triggers
+    { mode = "n", keys = "<Leader>" },
+    { mode = "x", keys = "<Leader>" },
+
+    -- Built-in completion
+    { mode = "i", keys = "<C-x>" },
+
+    -- `g` key
+    { mode = "n", keys = "g" },
+    { mode = "x", keys = "g" },
+
+    -- Marks
+    { mode = "n", keys = "'" },
+    { mode = "n", keys = "`" },
+    { mode = "x", keys = "'" },
+    { mode = "x", keys = "`" },
+
+    -- Registers
+    { mode = "n", keys = '"' },
+    { mode = "x", keys = '"' },
+    { mode = "i", keys = "<C-r>" },
+    { mode = "c", keys = "<C-r>" },
+
+    -- Window commands
+    { mode = "n", keys = "<C-w>" },
+
+    -- `z` key
+    { mode = "n", keys = "z" },
+    { mode = "x", keys = "z" },
+  },
+  window = {
+    delay = 200,
+    config = {
+      width = "auto",
+    },
+  },
+})
