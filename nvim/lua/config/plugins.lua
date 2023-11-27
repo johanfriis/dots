b005ea0ffc336b99ed4bde11520430b3108e9904
Johan Friis <johan@slowmail.it>
2023-11-27 22:29:30 +0100

    nvim: make mini.clue lazy

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -4,12 +4,12 @@ p.load('mini.align', {})
 p.load('mini.comment', {})
 p.lazy('mini.move', {})
 p.lazy('mini.pairs', {})
-p.load('cmp')
 
 p.lazy('copilot', nil, { 'InsertEnter' })
+p.load('cmp')
 p.load('lspconfig')
 
-p.load('mini-clue')
+p.lazy('mini-clue')
 
 vim.api.nvim_create_user_command('LazyGit', function()
   vim.api.nvim_del_user_command('LazyGit')
