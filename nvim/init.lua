6b760d09b39a3d8827c19e36b5d032a89471fef8
Johan Friis <johan@slowmail.it>
2023-11-29 16:32:27 +0100

    nvim: init align

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,14 +3,14 @@ vim.loader.enable()
 local g = vim.g
 
 -- Disable unused built-in plugins.
-g.loaded_gzip = true
-g.loaded_rplugin = true
-g.loaded_tarPlugin = true
-g.loaded_tohtml = true
-g.loaded_tutor = true
-g.loaded_zipPlugin = true
+g.loaded_gzip         = true
+g.loaded_rplugin      = true
+g.loaded_tarPlugin    = true
+g.loaded_tohtml       = true
+g.loaded_tutor        = true
+g.loaded_zipPlugin    = true
 g.loaded_editorconfig = true
-g.loaded_man = true
+g.loaded_man          = true
 
 
 require('config')
