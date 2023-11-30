4e2e2440210602b643cca88a08c94113a6d41590
Johan Friis <johan@slowmail.it>
2023-11-30 14:37:08 +0100

    nvim: autocmd to close some buffers with <q>

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,19 @@
+local f = require('utils.functions')
+
+--- Close selected buffers with <q>
+
+local closable = {
+  'help',
+  'startuptime'
+}
+
+f.autocmds('UserCloseWithQ', {{
+    events = "FileType",
+    pattern = closable,
+    command = [[
+      nnoremap <buffer><silent> q :close<CR>
+      set nobuflisted
+    ]],
+}})
+
+-- vim: foldmethod=marker ts=2 sts=2 sw=2 et
--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,4 +1,5 @@
 require('config.options')
+require('config.autocmds')
 require('config.colors')
 require('config.mappings')
 require('config.plugins')
