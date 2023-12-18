e4ebde1ee9803024c0c179ce25c462f463632469
Johan Friis <johan@slowmail.it>
2023-12-18 14:31:40 +0100

    nvim: lazy telekasten

--- nvim/lua/plugins/telekasten.lua
+++ nvim/lua/plugins/telekasten.lua
@@ -1,31 +1,33 @@
-local p = require('utils.pack')
-local tk = require('telekasten')
-
-p.add({
-  'telescope'
-})
+-- https://github.com/renerocksai/telekasten.nvim
 
 local notes = vim.fn.expand('~/dev/notes-tk')
 local weeklies = notes .. '/log'
 
-tk.setup({
-  home = notes,
-  command_palette_theme = 'dropdown',
-  show_tags_theme = 'get_cursor',
-  with_live_grep = true,
-  weeklies = function()
-    return weeklies .. '/' .. os.date('%Y')
-  end,
-  template_new_weekly = '.templates/weekly.md',
-  calendar_opts = {
-    weeknm = 1, --> WK01
-  },
+return {
+    {
+        'renerocksai/telekasten.nvim',
+        lazy = true,
+        cmd = { 'Telekasten' },
+        dependencies = {
+            'nvim-telescope/telescope.nvim',
+        },
+        opts = {
+            home = notes,
+            command_palette_theme = 'dropdown',
+            show_tags_theme = 'get_cursor',
+            with_live_grep = true,
+            weeklies = function()
+                return weeklies .. '/' .. os.date('%Y')
+            end,
+            template_new_weekly = '.templates/weekly.md',
+            calendar_opts = {
+                weeknm = 1, --> WK01
+            },
 
-  install_syntax = false,
-  take_over_my_home = false,
-  auto_set_filetype = false,
-  auto_set_syntax = false,
-
-})
-
-
+            install_syntax = false,
+            take_over_my_home = false,
+            auto_set_filetype = false,
+            auto_set_syntax = false,
+        }
+    },
+}
