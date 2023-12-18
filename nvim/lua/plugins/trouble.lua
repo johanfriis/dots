3b021ded674341516bbd62d7183cf2b554fcf7c8
Johan Friis <johan@slowmail.it>
2023-12-18 14:36:21 +0100

    nvim: lazy trouble

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,10 @@
+-- https://github.com/folke/trouble.nvim
+
+return {
+    {
+        'folke/trouble.nvim',
+        lazy = true,
+        cmd = { 'Trouble', 'TroubleToggle' },
+        config = true,
+    },
+}
