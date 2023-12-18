2e4429524b687d9239ba3da37c1ea0f05555ed6a
Johan Friis <johan@slowmail.it>
2023-12-18 15:07:49 +0100

    nvim: cleanup which-key and mappings

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,26 +1,17 @@
-local f = require('utils.functions')
-local map = f.map
-local leader = f.leader
-local pickers = require('utils.pickers')
+local map = require('utils.functions').map
 
-local M = {}
--- ============================================================================
--- mappings ===
+vim.g.mapleader = ' '
 
--- fix yanking and pasting
-map({'n', 'x'}, 'gy',  '"+y',  'Yank to system clipboard')
-map({'n', 'x'}, 'gyy', '"+yy', 'Yank to system clipboard')
-map('n',        'gp',  '"+p',  'Paste from system clipboard')
+--- ============================================================================
+--- core mappings ===
 
--- Paste in visual with P to not copy selected text (:h v_P)
-map('x', 'gp', '"+P',   'Paste from system clipboard')
-map("n", "gV", "`[v`]", 'Reselect pasted text')
+-- fix yanking and pasting - see (:h v_P) for explanation of second last map
+map({'n', 'x'}, 'gy', '"+y',   'Yank to system clipboard')
+map('n',        'gp', '"+p',   'Paste from system clipboard')
+map('x',        'gp', '"+P',   'Paste from system clipboard')
+map("n",        "gV", "`[v`]", 'Reselect pasted text')
 
--- Move by visible lines
-map({ 'n', 'x' }, 'j', [[v:count == 0 ? 'gj' : 'j']], { expr = true })
-map({ 'n', 'x' }, 'k', [[v:count == 0 ? 'gk' : 'k']], { expr = true })
-
--- Out / Indent
+-- In / Outdent
 map({ 'x' }, '<', "<gv", { nowait = true })
 map({ 'x' }, '>', ">gv", { nowait = true })
 
@@ -30,114 +21,23 @@ map({'i', 'c'}, '<C-f>', '<Right>', 'Right', { silent = false })
 map({'i', 'c'}, '<C-a>', '<Home>',  'Home',  { silent = false })
 map({'i', 'c'}, '<C-e>', '<End>',   'End',   { silent = false })
 
--- Disable `s` shortcut (use `cl` instead) for safer usage of 'mini.surround'
+-- Disable `s` shortcut (use `cl` instead), allowing `s` for "surround"
 map({'n', 'x'}, [[s]], [[<Nop>]])
 
--- Better command history navigation
+-- Use C-p/n for navigating command history
 map('c', '<C-p>', '<Up>',   { silent = false })
 map('c', '<C-n>', '<Down>', { silent = false })
 
-map('n', '§', "<Cmd>Telescope buffers<CR>", 'Buffers')
-
-map('n', ',e', "<Cmd>NvimTreeToggle<CR>",            '[E]xplore Tree')
-map('n', ',f', "<Cmd>Telescope find_files<CR>",      '[F]iles')
-map('n', ',r', [[<Cmd>Trouble lsp_references<CR>]],  '[R]eferences')
-map('n', ',d', [[<Cmd>Trouble lsp_definitions<CR>]], '[D]efinitions')
-
 -- This is such a nice and simple way of switching buffers,
--- I have to think a bit about it.
+-- I have to think a bit about whether I want to use it.
 -- nnoremap gb :buffers<CR>:buffer<Space>
+-- map('n', 'gb', ':buffers<CR>:buffer<Space>',   'Show buffers')
 
--- ============================================================================
--- leader mappings ===
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ','
+--- ============================================================================
+--- tool mappings ===
 
--- other good group names:
--- +Code, +Explore, +Find
-M.clues = {
-  { mode = 'n', keys = '<Leader>b', desc = '+Buffer' },
-  { mode = 'n', keys = '<Leader>d', desc = '+Dev' },
-  { mode = 'n', keys = '<Leader>g', desc = '+Goto' },
-  { mode = 'n', keys = '<Leader>j', desc = '+Journal' },
-  { mode = 'n', keys = '<Leader>n', desc = '+Notes' },
-  { mode = 'n', keys = '<Leader>p', desc = '+Pick' },
-  { mode = 'n', keys = '<Leader>t', desc = '+Toggle' },
+map('n', '§', ':lua require("telescope.builtin").buffers()<CR>')
 
-  { mode = 'x', keys = '<Leader>d', desc = '+Dev' },
-
-  { mode = 'n', keys = ',',         desc = '+Quick' },
-}
-
--- 'b' is for 'buffer'
-leader('n', 'ba', [[<Cmd>b#<CR>]],      '[A]lternate')
-leader('n', 'bs', f.new_scratch_buffer, '[S]cratch')
-leader('n', 'bq', [[<Cmd>close<CR>]],   '[Q]uit (close)')
-
--- 'd' is for 'dev'
-leader('x',          'da', [[gA]],                              '[A]lign selection')
-leader({ 'n', 'v' }, 'dc', vim.lsp.buf.code_action,             '[C]ode Action')
-leader({ 'n', 'v' }, 'dc', vim.lsp.buf.format,                  '[F]ormat')
-leader('n',          'dg', [[<Cmd>LazyGit<CR>]],                '[G]it Lazy (lazygit)')
-leader('n',          'dl', vim.diagnostic.open_float,           'F[l]oat Diagnostic')
-leader('n',          'dr', vim.lsp.buf.rename,                  '[R]ename')
-leader('n',          'ds', vim.lsp.buf.signature_help,          '[S]ignature help')
-leader('n',          'dS', [[<Cmd>write<CR><Cmd>source %<CR>]], '[S]ource file')
-
--- 'g' is for 'goto'
-leader('n', 'gd', [[<Cmd>Trouble lsp_definitions<CR>]],     '[D]efinitions')
-leader('n', 'gr', [[<Cmd>Trouble lsp_references<CR>]],      '[R]eferences')
-leader('n', 'gt', [[<Cmd>Trouble lsp_type_definition<CR>]], '[T]ype')
-
--- 'j' is for 'journal'
-leader('n', 'jc', [[<Cmd>Telekasten show_calendar<CR>]],     '[C]alendar')
-leader('n', 'jd', [[<Cmd>Telekasten goto_today<CR>]],        '[D]aily')
-leader('n', 'jD', [[<Cmd>Telekasten find_daily_notes<CR>]],  'Find [D]aily')
-leader('n', 'jo', [[<Cmd>Telekasten find_notes<CR>]],        '[O]pen')
-leader('n', 'jn', [[<Cmd>Telekasten new_note<CR>]],          '[N]ew')
-leader('n', 'js', [[<Cmd>Telekasten search_notes<CR>]],      '[S]earch')
-leader('n', 'jt', [[<Cmd>Telekasten show_tags<CR>]],         '[T]ags')
-leader('n', 'jv', [[<Cmd>Telekasten switch_vault<CR>]],      '[V]ault')
-leader('n', 'jw', [[<Cmd>Telekasten goto_thisweek<CR>]],     '[W]eekly')
-leader('n', 'jW', [[<Cmd>Telekasten find_weekly_notes<CR>]], 'Find [W]eekly')
-
--- 'p' is for 'pick'
-leader('n', 'pg', '<Cmd>Telescope git_files<CR>',                 '[G]it File')
-leader('n', 'pb', "<Cmd>Telescope file_browser<CR>",              '[B]rowse File')
-leader('n', 'ps', "<Cmd>Telescope live_grep<CR>",                 '[S]earch File')
-leader('n', 'pr', "<Cmd>Telescope oldfiles<CR>",                  '[R]ecent File')
-leader('n', 'pt', "<Cmd>Telescope current_buffer_fuzzy_find<CR>", '[T]his File')
-leader('n', 'pa', pickers.adjacent,                               '[A]djacent File')
-leader('n', 'pj', "<Cmd>Telescope whaler<CR>",                    '[J]ump Dir')
-leader('n', 'pu', "<Cmd>Telescope undo<CR>",                      '[U]ndo Tree')
-leader('n', 'nn', '<Cmd>Telekasten<CR>', 'Open Telekasten')
-
--- 't' is for 'toggle'
-leader('n', 'td',  f.toggle_diagnostic,                        'Toggle diagnostic')
-leader('n', 'th',  '<Cmd>let v:hlsearch = 1 - v:hlsearch<CR>', 'Toggle search highlight')
-leader('n', 'ti',  '<Cmd>setlocal ignorecase!<CR>',            "Toggle 'ignorecase'")
-leader('n', 'tl',  '<Cmd>setlocal list!<CR>',                  "Toggle 'list'")
-leader('n', 'tn',  '<Cmd>setlocal number!<CR>',                "Toggle 'number'")
-leader('n', 'tr',  '<Cmd>setlocal relativenumber!<CR>',        "Toggle 'relativenumber'")
-leader('n', 'ts',  '<Cmd>setlocal spell!<CR>',                 "Toggle 'spell'")
-leader('n', 'tw',  '<Cmd>setlocal wrap!<CR>',                  "Toggle 'wrap'")
-leader('n', 'tC',  f.toggle_colorcolumn,                       'Toggle colorcolumn')
-leader('n', 'tcl', '<Cmd>setlocal cursorline!<CR>',            "Toggle 'cursorline'")
-leader('n', 'tcc', '<Cmd>setlocal cursorcolumn!<CR>',          "Toggle 'cursorcolumn'")
-
--- g is for git
---  leader('n', 'gA', [[<Cmd>lua require("gitsigns").stage_buffer()<CR>]],        'Add buffer')
---  leader('n', 'ga', [[<Cmd>lua require("gitsigns").stage_hunk()<CR>]],          'Add (stage) hunk')
---  leader('n', 'gb', [[<Cmd>lua require("gitsigns").blame_line()<CR>]],          'Blame line')
--- leader('n', 'gg', [[<Cmd>lua open_lazygit()<CR>]],                          'Git tab')
---  leader('n', 'gp', [[<Cmd>lua require("gitsigns").preview_hunk()<CR>]],        'Preview hunk')
---  leader('n', 'gq', [[<Cmd>lua require("gitsigns").setqflist()<CR>:copen<CR>]], 'Quickfix hunks')
---  leader('n', 'gu', [[<Cmd>lua require("gitsigns").undo_stage_hunk()<CR>]],     'Undo stage hunk')
---  leader('n', 'gx', [[<Cmd>lua require("gitsigns").reset_hunk()<CR>]],          'Discard (reset) hunk')
---  leader('n', 'gX', [[<Cmd>lua require("gitsigns").reset_buffer()<CR>]],        'Discard (reset) buffer')
-
-return M
-
--- vim: foldmethod=marker ts=2 sts=2 sw=2 et
+-- vim: foldmethod=marker
 
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,58 +1,78 @@
 -- https://github.com/folke/which-key.nvim
+
 return {
     {
         'folke/which-key.nvim',
         event = 'VeryLazy',
         config = function()
             local wk = require('which-key')
+            local f = require('utils.functions')
             wk.setup({
                 show_help = false,
-                -- triggers_nowait = {
-                --   "<SPC>",
-                --   "g",
-                -- },
             })
             wk.register({
                 [','] = {
                     name = 'Fastkey',
-                    f = { ':Telescope find_files<CR>', 'Files' },
+                    e = { ':Explore<CR>',                 'Explore [Netrw]' },
+                    f = { ':Telescope find_files<CR>',    'Files [Pick]' },
+                    r = { ':Trouble lsp_references<CR>',  'References [LSP]' },
+                    d = { ':Trouble lsp_definitions<CR>', 'Definitions [LSP]' },
                 },
                 ['<leader>'] = {
                     b = {
                         name = 'Buffer',
                         a = { ':b#<CR>',    'Alternate' },
-                        q = { ':close<CR>', 'Qlose' },
                         h = { ':lua require("smart-list").swap_buf_left()<CR>',    'Swap Left' },
                         j = { ':lua require("smart-list").swap_buf_down()<CR>',    'Swap Down' },
                         k = { ':lua require("smart-list").swap_buf_up()<CR>',      'Swap Up' },
                         l = { ':lua require("smart-list").swap_buf_right()<CR>',   'Swap Right' },
+                        s = { f.new_scratch_buffer,                                'Scratch' },
                     },
                     d = {
                         name = 'Dev',
-                        g = { ':LazyGit<CR>',       'LazyGit' },
-                        v = { ':lua <C-r>"<CR>',    'Eval " register' },
+                        c = { vim.lsp.buf.code_action,    'Code Action [LSP]' },
+                        d = { vim.diagnostic.open_float,  'Diagnostics [LSP]' },
+                        f = { vim.lsp.buf.format,         'Format [LSP]' },
+                        g = { ':LazyGit<CR>',             'LazyGit' },
+                        r = { vim.lsp.buf.rename,         'Rename [LSP]' },
+                        s = { vim.lsp.buf.signature_help, 'Signature Help [LSP]' },
+                        v = { ':lua <C-r>"<CR>',          'Eval " register' },
+                    },
+                    j = {
+                      name = 'Journal',
+                      c = { ':Telekasten show_calendar<CR>',     'Calendar' },
+                      d = { ':Telekasten goto_today<CR>',        'Daily' },
+                      D = { ':Telekasten find_daily_notes<CR>',  'Find Daily' },
+                      o = { ':Telekasten find_notes<CR>',        'Open' },
+                      n = { ':Telekasten new_note<CR>',          'New' },
+                      s = { ':Telekasten search_notes<CR>',      'Search' },
+                      t = { ':Telekasten show_tags<CR>',         'Tags' },
+                      v = { ':Telekasten switch_vault<CR>',      'Vault' },
+                      w = { ':Telekasten goto_thisweek<CR>',     'Weekly' },
+                      W = { ':Telekasten find_weekly_notes<CR>', 'Find Weekly' },
                     },
                     p = {
                         name = 'Pick',
                         a = { ':lua require("utils.pickers").adjacent()<CR>', 'Adjacent File' },
-                        g = { ':Telescope git_files<CR>', 'Git File' },
-                        s = { ':Telescope live_grep<CR>', 'Search File' },
-                        r = { ':Telescope oldfiles<CR>',  'Recent File' },
-                        t = { ':Telescope current_buffer_fuzzy_finder<CR>', 'This File' },
-                        v = { ':Telescope help_tags<CR>', 'Vim Help' },
+                        g = { ':Telescope git_files<CR>',                     'Git File' },
+                        s = { ':Telescope live_grep<CR>',                     'Search File' },
+                        r = { ':Telescope oldfiles<CR>',                      'Recent File' },
+                        t = { ':Telescope current_buffer_fuzzy_finder<CR>',   'This File' },
+                        u = { ':Telescope undo<CR>',                          'Undotree' },
+                        v = { ':Telescope help_tags<CR>',                     'Vim Help' },
                     },
                     t = {
                         name = 'Toggle',
-                        d = { ':lua require("utils.functions").toggle_diagnostic()<CR>', 'Toggle Diagnostic' },
-                        h = { ':setlocal hlsearch!<CR>',   'Toggle `hlsearch`' },
-                        i = { ':setlocal ignorecase!<CR>', 'Toggle `ignorecase`' },
-                        l = { ':setlocal list!<CR>', 'Toggle `list`' },
-                        n = { ':setlocal number!<CR>', 'Toggle `number`' },
-                        r = { ':setlocal relativenumber!<CR>', 'Toggle `relativenumber`' },
-                        s = { ':setlocal spell!<CR>', 'Toggle `spell`' },
-                        w = { ':setlocal wrap!<CR>', 'Toggle `wrap`' },
-                        c = { ':setlocal cursorline!<CR>', 'Toggle `cursorline`' },
-                        v = { ':setlocal cursorcolumn!<CR>', 'Toggle `cursorcolumn`' },
+                        d = { ':lua require("utils.functions").toggle_diagnostic()<CR>',  'Toggle Diagnostic' },
+                        h = { ':setlocal hlsearch!<CR>',                                  'Toggle `hlsearch`' },
+                        i = { ':setlocal ignorecase!<CR>',                                'Toggle `ignorecase`' },
+                        l = { ':setlocal list!<CR>',                                      'Toggle `list`' },
+                        n = { ':setlocal number!<CR>',                                    'Toggle `number`' },
+                        r = { ':setlocal relativenumber!<CR>',                            'Toggle `relativenumber`' },
+                        s = { ':setlocal spell!<CR>',                                     'Toggle `spell`' },
+                        w = { ':setlocal wrap!<CR>',                                      'Toggle `wrap`' },
+                        c = { ':setlocal cursorline!<CR>',                                'Toggle `cursorline`' },
+                        v = { ':setlocal cursorcolumn!<CR>',                              'Toggle `cursorcolumn`' },
                         x = { ':lua require("utils.functions").toggle_colorcolumn()<CR>', 'Toggle colorcolumn' },
                     },
                 },
@@ -61,74 +81,5 @@ return {
     },
 }
 
-
--- 
--- -- 'd' is for 'dev'
--- leader('x',          'da', [[gA]],                              '[A]lign selection')
--- leader({ 'n', 'v' }, 'dc', vim.lsp.buf.code_action,             '[C]ode Action')
--- leader({ 'n', 'v' }, 'dc', vim.lsp.buf.format,                  '[F]ormat')
--- leader('n',          'dg', [[<Cmd>LazyGit<CR>]],                '[G]it Lazy (lazygit)')
--- leader('n',          'dl', vim.diagnostic.open_float,           'F[l]oat Diagnostic')
--- leader('n',          'dr', vim.lsp.buf.rename,                  '[R]ename')
--- leader('n',          'ds', vim.lsp.buf.signature_help,          '[S]ignature help')
--- leader('n',          'dS', [[<Cmd>write<CR><Cmd>source %<CR>]], '[S]ource file')
-
--- leader('n', 'pb', "<Cmd>Telescope file_browser<CR>",              '[B]rowse File')
--- leader('n', 'pj', "<Cmd>Telescope whaler<CR>",                    '[J]ump Dir')
--- leader('n', 'pu', "<Cmd>Telescope undo<CR>",                      '[U]ndo Tree')
--- leader('n', 'nn', '<Cmd>Telekasten<CR>', 'Open Telekasten')
-
--- map('n', ',e', "<Cmd>NvimTreeToggle<CR>",            '[E]xplore Tree')
--- map('n', ',r', [[<Cmd>Trouble lsp_references<CR>]],  '[R]eferences')
--- map('n', ',d', [[<Cmd>Trouble lsp_definitions<CR>]], '[D]efinitions')
-
---   { mode = 'n', keys = '<Leader>b', desc = '+Buffer' },
---   { mode = 'n', keys = '<Leader>d', desc = '+Dev' },
---   { mode = 'n', keys = '<Leader>g', desc = '+Goto' },
---   { mode = 'n', keys = '<Leader>j', desc = '+Journal' },
---   { mode = 'n', keys = '<Leader>n', desc = '+Notes' },
---   { mode = 'n', keys = '<Leader>p', desc = '+Pick' },
---   { mode = 'n', keys = '<Leader>t', desc = '+Toggle' },
--- 
---   { mode = 'x', keys = '<Leader>d', desc = '+Dev' },
--- 
---   { mode = 'n', keys = ',',         desc = '+Quick' },
-
--- -- 'b' is for 'buffer'
--- leader('n', 'ba', [[<Cmd>b#<CR>]],      '[A]lternate')
--- leader('n', 'bs', f.new_scratch_buffer, '[S]cratch')
--- leader('n', 'bq', [[<Cmd>close<CR>]],   '[Q]uit (close)')
--- 
--- -- 'g' is for 'goto'
--- leader('n', 'gd', [[<Cmd>Trouble lsp_definitions<CR>]],     '[D]efinitions')
--- leader('n', 'gr', [[<Cmd>Trouble lsp_references<CR>]],      '[R]eferences')
--- leader('n', 'gt', [[<Cmd>Trouble lsp_type_definition<CR>]], '[T]ype')
--- 
--- -- 'j' is for 'journal'
--- leader('n', 'jc', [[<Cmd>Telekasten show_calendar<CR>]],     '[C]alendar')
--- leader('n', 'jd', [[<Cmd>Telekasten goto_today<CR>]],        '[D]aily')
--- leader('n', 'jD', [[<Cmd>Telekasten find_daily_notes<CR>]],  'Find [D]aily')
--- leader('n', 'jo', [[<Cmd>Telekasten find_notes<CR>]],        '[O]pen')
--- leader('n', 'jn', [[<Cmd>Telekasten new_note<CR>]],          '[N]ew')
--- leader('n', 'js', [[<Cmd>Telekasten search_notes<CR>]],      '[S]earch')
--- leader('n', 'jt', [[<Cmd>Telekasten show_tags<CR>]],         '[T]ags')
--- leader('n', 'jv', [[<Cmd>Telekasten switch_vault<CR>]],      '[V]ault')
--- leader('n', 'jw', [[<Cmd>Telekasten goto_thisweek<CR>]],     '[W]eekly')
--- leader('n', 'jW', [[<Cmd>Telekasten find_weekly_notes<CR>]], 'Find [W]eekly')
--- 
-
--- g is for git
---  leader('n', 'gA', [[<Cmd>lua require("gitsigns").stage_buffer()<CR>]],        'Add buffer')
---  leader('n', 'ga', [[<Cmd>lua require("gitsigns").stage_hunk()<CR>]],          'Add (stage) hunk')
---  leader('n', 'gb', [[<Cmd>lua require("gitsigns").blame_line()<CR>]],          'Blame line')
--- leader('n', 'gg', [[<Cmd>lua open_lazygit()<CR>]],                          'Git tab')
---  leader('n', 'gp', [[<Cmd>lua require("gitsigns").preview_hunk()<CR>]],        'Preview hunk')
---  leader('n', 'gq', [[<Cmd>lua require("gitsigns").setqflist()<CR>:copen<CR>]], 'Quickfix hunks')
---  leader('n', 'gu', [[<Cmd>lua require("gitsigns").undo_stage_hunk()<CR>]],     'Undo stage hunk')
---  leader('n', 'gx', [[<Cmd>lua require("gitsigns").reset_hunk()<CR>]],          'Discard (reset) hunk')
---  leader('n', 'gX', [[<Cmd>lua require("gitsigns").reset_buffer()<CR>]],        'Discard (reset) buffer')
-
--- return M
-
--- vim: foldmethod=marker ts=2 sts=2 sw=2 et
+-- vim: foldmethod=marker
 
