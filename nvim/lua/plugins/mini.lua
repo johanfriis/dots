48e29cd80d1be19c6869875e8c1b73e6a770b3d7
Johan Friis <johan@slowmail.it>
2023-12-18 14:37:29 +0100

    nvim: lazy mini

---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        'echasnovski/mini.align',
+        version = false,
+        lazy = true,
+        event = { 'BufReadPre', 'BufNewFile' },
+        opts = {},
+    },
+    {
+        'echasnovski/mini.comment',
+        version = false,
+        lazy = true,
+        event = { 'BufReadPre', 'BufNewFile' },
+        opts = {},
+    },
+    {
+        'echasnovski/mini.pairs',
+        version = false,
+        lazy = true,
+        event = { 'BufReadPre', 'BufNewFile' },
+        opts = {},
+    },
+    {
+        'echasnovski/mini.surround',
+        version = false,
+        lazy = true,
+        event = { 'BufReadPre', 'BufNewFile' },
+        opts = {},
+    },
+}
