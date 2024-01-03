d35d9c0e4f0a5c79763ecec7c00c98723904762c
Johan Friis <johan@slowmail.it>
2024-01-03 17:52:14 +0100

    nvim: add mini-jump2d

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -2,28 +2,38 @@ return {
     {
         'echasnovski/mini.align',
         version = false,
-        lazy = true,
         event = { 'BufReadPre', 'BufNewFile' },
         opts = {},
     },
     {
         'echasnovski/mini.comment',
         version = false,
-        lazy = true,
         event = { 'BufReadPre', 'BufNewFile' },
         opts = {},
     },
     {
+        'echasnovski/mini.jump2d',
+        version = false,
+        event = { 'BufReadPre', 'BufNewFile' },
+        opts = {
+            view = {
+                dim = true,
+            },
+            labels = 'abcdefghijklmnopqrstuvwxyz',
+            mappings = {
+                start_jumping = '<Tab>',
+            },
+        },
+    },
+    {
         'echasnovski/mini.pairs',
         version = false,
-        lazy = true,
         event = { 'BufReadPre', 'BufNewFile' },
         opts = {},
     },
     {
         'echasnovski/mini.surround',
         version = false,
-        lazy = true,
         event = { 'BufReadPre', 'BufNewFile' },
         opts = {},
     },
