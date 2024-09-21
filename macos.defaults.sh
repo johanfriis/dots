5ce3aae630c8fad4330f09b577c8bef585a81f68
Johan Friis <johan@slowmail.it>
2024-09-21 14:09:05 +0200

    macos: decrease KeyRepeat

--- macos.defaults.sh
+++ macos.defaults.sh
@@ -1,8 +1,8 @@
 # fast key repeat
 # Test here: https://mac-os-key-repeat.vercel.app
 # Once set, enable by calling `killall Finder`
-defaults write -g InitialKeyRepeat -int 12
-defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 13
+  defaults write -g KeyRepeat -int 2
 
 # disable press-and-hold
 defaults write -g ApplePressAndHoldEnabled -bool false
